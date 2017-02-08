13509529cf04caafa1a14c0a45c522a7f38f09bb
Silence <374619540@qq.com>
2017-02-08 21:18:35 +0800

    3DTouch

--- AppInfo/AppDelegate.m
+++ AppInfo/AppDelegate.m
@@ -17,7 +17,15 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     return YES;
 }
-
-
+/// 3DTouch
+- (void)application:(UIApplication *)application performActionForShortcutItem:(UIApplicationShortcutItem *)shortcutItem completionHandler:(void (^)(BOOL))completionHandler{
+    if([shortcutItem.type isEqualToString:@"Share"]){
+        NSArray *items = @[[NSURL URLWithString:@"https://github.com/silence0201/AppInfo"], [UIImage imageNamed:@"AppIcon60x60@3x.png"] ];
+        UIActivityViewController *controller = [[UIActivityViewController alloc]initWithActivityItems:items applicationActivities:nil] ;
+        [[[[UIApplication sharedApplication]keyWindow]rootViewController ] presentViewController:controller animated:YES completion:nil] ;
+    }else if([shortcutItem.type isEqualToString:@"GitHub"]){
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://github.com/silence0201/AppInfo"]] ;
+    }
+}
 
 @end
